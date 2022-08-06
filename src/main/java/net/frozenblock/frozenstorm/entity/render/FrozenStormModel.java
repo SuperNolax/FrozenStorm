ae719147fbd07ba4b7bd59bdb6916fc9b0e775e5
Treetrain1 <treetrain1@gmail.com>
2022-08-05 19:27:18 -0500

    model

--- src/main/java/net/frozenblock/frozenstorm/entity/render/FrozenStormModel.java
+++ src/main/java/net/frozenblock/frozenstorm/entity/render/FrozenStormModel.java
@@ -21,53 +21,50 @@ public class FrozenStormModel<T extends FrozenStormEntity> extends SinglePartEnt
     private final ModelPart spike2;
     private final ModelPart spike1;
     private final ModelPart spike3;
-    private final ModelPart body1;
-    private final ModelPart body2;
-    private final ModelPart body3;
-    private final ModelPart head1;
-    private final ModelPart head2;
-    private final ModelPart head3;
+    private final ModelPart ribcage;
+    private final ModelPart tail;
+    private final ModelPart centerHead;
+    private final ModelPart rightHead;
+    private final ModelPart leftHead;
 
     public FrozenStormModel(ModelPart root) {
         this.root = root;
+        this.ribcage = root.getChild("ribcage");
+        this.tail = root.getChild(EntityModelPartNames.TAIL);
+        this.centerHead = root.getChild("center_head");
+        this.rightHead = root.getChild("right_head");
+        this.leftHead = root.getChild("left_head");
         this.spike2 = root.getChild("spike2");
         this.spike1 = root.getChild("spike1");
         this.spike3 = root.getChild("spike3");
-        this.body1 = root.getChild("body1");
-        this.body2 = root.getChild("body2");
-        this.body3 = root.getChild("body3");
-        this.head1 = root.getChild("head1");
-        this.head2 = root.getChild("head2");
-        this.head3 = root.getChild("head3");
     }
 
     public static TexturedModelData getTexturedModelData() {
         ModelData modelData = new ModelData();
         ModelPartData modelPartData = modelData.getRoot();
-        modelPartData.addChild("shoulders", ModelPartBuilder.create().uv(0, 16).cuboid(-10.0F, 3.9F, -0.5F, 20.0F, 3.0F, 3.0F, new Dilation(0.5F)), ModelTransform.NONE);
-        float f = 0.20420352F;
-        modelPartData.addChild(
-                "ribcage",
-                ModelPartBuilder.create()
-                        .uv(0, 22)
-                        .cuboid(0.0F, 0.0F, 0.0F, 3.0F, 10.0F, 3.0F, new Dilation(0.5F))
-                        .uv(24, 22)
-                        .cuboid(-4.0F, 1.5F, 0.5F, 11.0F, 2.0F, 2.0F, new Dilation(0.5F))
-                        .uv(24, 22)
-                        .cuboid(-4.0F, 4.0F, 0.5F, 11.0F, 2.0F, 2.0F, new Dilation(0.5F))
-                        .uv(24, 22)
-                        .cuboid(-4.0F, 6.5F, 0.5F, 11.0F, 2.0F, 2.0F, new Dilation(0.5F)),
-                ModelTransform.of(-2.0F, 6.9F, -0.5F, 0.20420352F, 0.0F, 0.0F)
-        );
-        modelPartData.addChild(
-                "tail",
-                ModelPartBuilder.create().uv(12, 22).cuboid(0.0F, 0.0F, 0.0F, 3.0F, 6.0F, 3.0F, new Dilation(0.5F)),
-                ModelTransform.of(-2.0F, 6.9F + MathHelper.cos(0.20420352F) * 10.0F, -0.5F + MathHelper.sin(0.20420352F) * 10.0F, 0.83252203F, 0.0F, 0.0F)
-        );
-        modelPartData.addChild("center_head", ModelPartBuilder.create().uv(0, 0).cuboid(-4.0F, -4.0F, -4.0F, 8.0F, 8.0F, 8.0F, new Dilation(0.5F)), ModelTransform.NONE);
-        ModelPartBuilder modelPartBuilder = ModelPartBuilder.create().uv(32, 0).cuboid(-4.0F, -4.0F, -4.0F, 6.0F, 6.0F, 6.0F, new Dilation(0.5F));
-        modelPartData.addChild("right_head", modelPartBuilder, ModelTransform.pivot(-8.0F, 4.0F, 0.0F));
-        modelPartData.addChild("left_head", modelPartBuilder, ModelTransform.pivot(10.0F, 4.0F, 0.0F));
+
+        ModelPartData spike2 = modelPartData.addChild("spike2", ModelPartBuilder.create().uv(12, 45).cuboid(-16.0F, -32.0F, -1.0F, 6.0F, 8.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(7.0F, 24.0F, 0.0F, 0.0F, 0.0F, -0.1309F));
+
+        ModelPartData spike1 = modelPartData.addChild("spike1", ModelPartBuilder.create().uv(18, 34).cuboid(-4.0F, -42.0F, -1.0F, 8.0F, 11.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(4.0F, 24.0F, 0.0F, 0.0F, 0.0F, -0.1309F));
+
+        ModelPartData spike3 = modelPartData.addChild("spike3", ModelPartBuilder.create().uv(0, 44).cuboid(6.0F, -27.0F, -2.0F, 6.0F, 12.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(-3.0F, 17.0F, 0.0F, 0.0F, 0.0F, 0.2182F));
+
+        ModelPartData shoulders = modelPartData.addChild("shoulders", ModelPartBuilder.create().uv(0, 0).cuboid(-10.0F, 3.9F, -0.5F, 20.0F, 3.0F, 3.0F, new Dilation(0.0F))
+                .uv(0, 34).cuboid(-3.0F, -2.0F, -0.51F, 6.0F, 7.0F, 3.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+
+        ModelPartData ribcage = modelPartData.addChild("ribcage", ModelPartBuilder.create().uv(34, 34).cuboid(0.0F, 0.0F, 0.0F, 3.0F, 10.0F, 3.0F, new Dilation(0.0F))
+                .uv(32, 14).cuboid(-4.0F, 1.5F, 0.5F, 11.0F, 2.0F, 2.0F, new Dilation(0.0F))
+                .uv(24, 10).cuboid(-4.0F, 4.0F, 0.5F, 11.0F, 2.0F, 2.0F, new Dilation(0.0F))
+                .uv(24, 6).cuboid(-4.0F, 6.5F, 0.5F, 11.0F, 2.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(-2.0F, 6.9F, -0.5F));
+
+        ModelPartData tail = modelPartData.addChild(EntityModelPartNames.TAIL, ModelPartBuilder.create().uv(42, 18).cuboid(0.0F, 0.0F, 0.0F, 3.0F, 6.0F, 3.0F, new Dilation(0.0F)), ModelTransform.pivot(-2.0F, 16.9F, -0.5F));
+
+        ModelPartData center_head = modelPartData.addChild("center_head", ModelPartBuilder.create().uv(0, 6).cuboid(-4.0F, -10.0F, -4.0F, 8.0F, 8.0F, 8.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+
+        ModelPartData right_head = modelPartData.addChild("right_head", ModelPartBuilder.create().uv(24, 22).cuboid(-4.0F, -4.0F, -4.0F, 6.0F, 6.0F, 6.0F, new Dilation(0.0F)), ModelTransform.pivot(-8.0F, 4.0F, 0.0F));
+
+        ModelPartData left_head = modelPartData.addChild("left_head", ModelPartBuilder.create().uv(0, 22).cuboid(-4.0F, -4.0F, -4.0F, 6.0F, 6.0F, 6.0F, new Dilation(0.0F)), ModelTransform.pivot(10.0F, 4.0F, 0.0F));
+
         return TexturedModelData.of(modelData, 64, 64);
     }
 
